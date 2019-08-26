9ad5ee0dcac51080e685b19598f85150ba3ea28d
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 18:27:51 +0700

    add generator

--- blocks/blocks_temp.js
+++ blocks/blocks_temp.js
@@ -1,15 +1,19 @@
-Blockly.Blocks['ky_001_temp_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-001 TEMP");
-    this.appendValueInput("KY_001_TEMP_DATA_PIN")
-        .setCheck("Number")
-        .appendField("DATA PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_001_temp_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-001 TEMP");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_001_TEMP_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_001_TEMP_VALUE"),
+          "KY_001_TEMP_VALUE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_temp.js
+++ blocks/generators_temp.js
@@ -1,6 +1,36 @@
-Blockly.JavaScript['ky_001_temp_block'] = function(block) {
-  var value_ky_001_temp_data_pin = Blockly.JavaScript.valueToCode(block, 'KY_001_TEMP_DATA_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_001_temp_block"] = function(block) {
+    var text_ky_001_temp_pin = block.getFieldValue("KY_001_TEMP_PIN");
+    var variable_ky_001_temp_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_001_TEMP_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+
+	#EXTINC
+	#include <OneWire.h>
+	#include <DallasTemperature.h>
+	#END
+
+    #VARIABLE
+
+	// Data wire is plugged into pin 2 on the Arduino
+	#define ONE_WIRE_BUS ${text_ky_001_temp_pin}
+
+	// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+	OneWire oneWire(ONE_WIRE_BUS);
+	// Pass our oneWire reference to Dallas Temperature. 
+	DallasTemperature sensors(&oneWire);
+	#END
+
+	#SETUP
+	  // Start up the library
+	  sensors.begin(); // IC Default 9 bit. If you have troubles consider upping it 12. Ups the delay giving the IC more time to process the temperature measurement
+	#END
+
+		sensors.requestTemperatures(); // Send the command to get temperatures
+	  ${variable_ky_001_temp_value} = sensors.getTempCByIndex(0);
+	
+    `;
+    return code;
+  };
